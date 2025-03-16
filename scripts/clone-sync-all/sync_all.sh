f6db618192e574399b604cd1f760fd1d15734f2f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2025-03-16 21:31:03 +0200

    -- script properly handles empty value for the repo name

--- scripts/clone-sync-all/sync_all.sh
+++ scripts/clone-sync-all/sync_all.sh
@@ -2,6 +2,7 @@
 FILE="repos.txt"
 
 while IFS=$'\t' read -r id url; do
+  if [ -n "$url" ]; then
     if [ -d "$id" ]; then
         echo "Updating $id..."
         cd "$id" || exit
@@ -12,6 +13,7 @@ while IFS=$'\t' read -r id url; do
         echo "Cloning $id..."
         git clone "$url" "$id"
     fi
+  fi
 done < "$FILE"
 
 echo "done"
